118d330fe042466900beca4c3ce976c409acc3b5
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-01-16 16:04:28 +0100

    dev: added library

---
+++ 02-emscripten/02-library/wrapper.js
@@ -0,0 +1,11 @@
+// @ts-nocheck
+const assert = require("assert");
+const Module = require("./dist/add.js");
+
+Module.onRuntimeInitialized = () => {
+  const a = 2.3;
+  const b = 4.1;
+  const result = Module._add(a, b);
+  assert.equal(a + b, result);
+  console.log(`${a} + ${b} = ${result}`);
+};
