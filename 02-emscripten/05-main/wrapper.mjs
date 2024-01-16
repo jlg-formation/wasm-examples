dd6067057e2e24d58d585dfbcea690eae2fe59c5
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-01-16 17:49:17 +0100

    dev: main working

--- 02-emscripten/05-main/wrapper.mjs
+++ 02-emscripten/05-main/wrapper.mjs
@@ -6,8 +6,19 @@ const instance = await createModule({
   printErr: () => {},
 });
 
+const makeMainArgs = (array) => {
+  // ASSUMPTION: pointer size is 4 bytes
+  const ptr = instance._malloc(array.length * 4);
+  for (let i = 0; i < array.length; i++) {
+    const cstr = instance.stringToNewUTF8(array[i]);
+    instance.setValue(ptr + i * 4, cstr, "i32");
+  }
+  return { argv: ptr, argc: array.length };
+};
+
 const test1 = () => {
-  const result = instance._main();
+  const { argv, argc } = makeMainArgs(["truc", "bidule"]);
+  const result = instance._main(argc, argv);
   console.log("result: ", result);
   equal(result, 123);
 };
