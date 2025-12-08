8827ccbf81c44a0a8aec5cf90c7388c9aa6e3051
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-12-08 20:06:50 +0100

    dev: update Makefile to use variable for node command and improve wrapper.js logging

--- 02-emscripten/02-library/wrapper.js
+++ 02-emscripten/02-library/wrapper.js
@@ -5,6 +5,7 @@ const Module = require("./dist/add.js");
 Module.onRuntimeInitialized = () => {
   const a = 2.3;
   const b = 4.1;
+  console.log("Module: ", Module);
   const result = Module._add(a, b);
   assert.equal(a + b, result);
   console.log(`${a} + ${b} = ${result}`);
