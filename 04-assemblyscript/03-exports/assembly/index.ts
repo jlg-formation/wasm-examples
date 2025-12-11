4e469b09e285d331903ac4375692d0f43faa6d97
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-12-11 14:03:12 +0000

    dev: add export for variable 'b' and update related files to reflect changes

--- 04-assemblyscript/03-exports/assembly/index.ts
+++ 04-assemblyscript/03-exports/assembly/index.ts
@@ -1,5 +1,7 @@
 export const a = 1234;
 
+export let b = 6789;
+
 export const myArray = new ArrayBuffer(24);
 const myPrivateTypedArray = Int32Array.wrap(myArray);
 myPrivateTypedArray[0] = 56;
--- 04-assemblyscript/03-exports/tests/index.js
+++ 04-assemblyscript/03-exports/tests/index.js
@@ -1,5 +1,7 @@
-import { a, myArray } from "../build/debug.js";
+import { a, b, myArray, myStr } from "../build/debug.js";
 
+console.log("myStr: ", myStr.value);
 console.log("a: ", a.value);
+console.log("b: ", b.value);
 const myTypedArray = new Uint32Array(myArray.value);
 console.log("myTypedArray[0]: ", myTypedArray[0]);
